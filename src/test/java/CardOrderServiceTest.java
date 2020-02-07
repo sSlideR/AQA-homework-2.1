bdbc7765f5b94cf5fe8065f0da5d04e3cb67201b
alex <alex92471@gmail.com>
2020-02-07 19:16:06 +0300

    appveyor modified

--- src/test/java/CardOrderServiceTest.java
+++ src/test/java/CardOrderServiceTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
