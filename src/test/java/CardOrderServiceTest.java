8e4789d4b717810861e74f07e25ba153cf2c2bc5
alex <alex92471@gmail.com>
2020-02-07 19:28:53 +0300

    Tests Modified

--- src/test/java/CardOrderServiceTest.java
+++ src/test/java/CardOrderServiceTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.*;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,13 +10,14 @@ import static com.codeborne.selenide.Condition.*;
 
 public class CardOrderServiceTest {
 
-    @BeforeEach
-    void InitialSetup() {
-        open("http://localhost:9999");
+    @AfterEach
+    void closeBrowser() {
+        close();
     }
 
     @Test
     void ShouldSubmitForm() {
+        open("http://localhost:9999");
         SelenideElement form = $("form");
         form.$("input.input__control[name=name").setValue("Петров Иван");
         form.$("input.input__control[name=phone").setValue("+79210001234");
@@ -23,12 +25,12 @@ public class CardOrderServiceTest {
         form.$("button").click();
         System.out.println($(".Success_successBlock__2L3Cw span").getText());
         $(".Success_successBlock__2L3Cw p").shouldHave(exactText("Ваша заявка успешно123 отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "testdata.csv",numLinesToSkip = 1,delimiter = ';',encoding = "Windows-1251")
     void ShouldAlertWhenInvalidInput(String name, String phone, String message_name, String message_phone) {
+        open("http://localhost:9999");
         SelenideElement form = $("form");
         form.$("input.input__control[name=name").setValue(name);
         form.$("input.input__control[name=phone").setValue(phone);
